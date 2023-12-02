5d236f8ea24e82441d45a73380cc83eba7c0a212
enmohamedMallahi <mohaxxmall@ygmail.com>
2023-12-02 19:27:01 +0100

    fix: bug

--- src/components/CarsList.js
+++ src/components/CarsList.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const CarsList = ({ cars, openModal, showBookButton }) => {
   return (
     <ul className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-4 md:px-8 px-4">
