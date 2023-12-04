dd5bef0ed77eedca6590265d50e2f0b322d0f3d0
enmohamedMallahi <mohaxxmall@ygmail.com>
2023-12-04 13:11:09 +0100

    feature: step handling for booking

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const ConfirmationModal = ({ carDetails, handleClose, bookingData }) => {
+  const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({
     customerName: '',
     customerEmail: '',
@@ -19,6 +20,10 @@ const ConfirmationModal = ({ carDetails, handleClose, bookingData }) => {
       ...formData,
       [name]: value,
     });
+  };
+
+  const handleNext = () => {
+    setStep(step + 1);
   };
 
   const handleBook = async () => {
@@ -56,115 +61,124 @@ const ConfirmationModal = ({ carDetails, handleClose, bookingData }) => {
     }
   };
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 ">
-      <div className="bg-white mx-4 p-8 rounded-md">
+   return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white p-8 rounded-md max-h-[80vh] overflow-y-auto">
         <h2 className="text-2xl font-bold mb-4">Confirm Booking</h2>
-        <p>{`You are about to book ${ carDetails.make } ${ carDetails.model }`}</p>
 
-        {/* Customer Information */}
-        <label htmlFor="customerName" className="block ">
-          Customer Name:
-          <input
-            type="text"
-            id="customerName"
-            name="customerName"
-            value={formData.customerName}
-            onChange={handleChange}
-            className="border border-gray-300 p-2 w-full"
-          />
-        </label>
+        {step === 1 && (
+          <div className="space-y-4">
+            {/* Personal Information Form */}
+            <label htmlFor="customerName">
+              <span className="text-sm">Customer Name:</span>
+              <input
+                type="text"
+                id="customerName"
+                name="customerName"
+                value={formData.customerName}
+                onChange={handleChange}
+                className="border border-gray-300 p-2 w-full"
+              />
+            </label>
 
-        <div className="flex gap-4 mb-4">
+            <label htmlFor="customerEmail">
+              <span className="text-sm">Customer Email:</span>
+              <input
+                type="email"
+                id="customerEmail"
+                name="customerEmail"
+                value={formData.customerEmail}
+                onChange={handleChange}
+                className="border border-gray-300 p-2 w-full"
+              />
+            </label>
 
-          <label htmlFor="customerEmail" className="block mt-4">
-            Customer Email:
-            <input
-              type="email"
-              id="customerEmail"
-              name="customerEmail"
-              value={formData.customerEmail}
-              onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
-            />
-          </label>
+            <label htmlFor="customerPhone">
+              <span className="text-sm">Customer Phone:</span>
+              <input
+                type="tel"
+                id="customerPhone"
+                name="customerPhone"
+                value={formData.customerPhone}
+                onChange={handleChange}
+                className="border border-gray-300 p-2 w-full"
+              />
+            </label>
 
-          <label htmlFor="customerPhone" className="block mt-4">
-            Customer Phone:
-            <input
-              type="tel"
-              id="customerPhone"
-              name="customerPhone"
-              value={formData.customerPhone}
-              onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
-            />
-          </label>
+            <div className="flex justify-end mt-4">
+              <button onClick={handleClose} className="mr-2 px-4 py-2 border border-gray-300 rounded-md">
+                Cancel
+              </button>
+              <button onClick={handleNext} className="px-4 py-2 bg-blue-500 text-white rounded-md">
+                Next
+              </button>
+            </div>
+          </div>
+        )}
 
-        </div>
+        {step === 2 && (
+          <div className="space-y-4">
+            {/* Booking Dates and Places Form */}
+            <label htmlFor="pickupDate">
+              <span className="text-sm">Pickup Date:</span>
+              <input
+                type="date"
+                id="pickupDate"
+                name="pickupDate"
+                value={formData.pickupDate}
+                onChange={handleChange}
+                className="border border-gray-300 p-2 w-full"
+              />
+            </label>
 
-        {/* Booking Information */}
-        <div className="flex gap-4 mb-4">
-          <label htmlFor="pickupDate" className="block w-full ">
-            Pickup Date:
-            <input
-              type="date"
-              id="pickupDate"
-              name="pickupDate"
-              value={formData.pickupDate}
-              onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
-            />
-          </label>
-          <label htmlFor="dropoffDate" className="block w-full ">
-            Dropoff Date:
-            <input
-              type="date"
-              id="dropoffDate"
-              name="dropoffDate"
-              value={formData.dropoffDate}
-              onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
-            />
-          </label>
-        </div>
+            <label htmlFor="dropoffDate">
+              <span className="text-sm">Dropoff Date:</span>
+              <input
+                type="date"
+                id="dropoffDate"
+                name="dropoffDate"
+                value={formData.dropoffDate}
+                onChange={handleChange}
+                className="border border-gray-300 p-2 w-full"
+              />
+            </label>
 
-        <div className="flex  gap-4 mb-4">
-          <label htmlFor="pickupPlace" className="block w-full ">
-            Pickup Place:
-            <input
-              type="text"
-              id="pickupPlace"
-              name="pickupPlace"
-              value={formData.pickupPlace}
-              onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
-            />
-          </label>
-          <label htmlFor="dropoffPlace" className="block w-full ">
-            Dropoff Place:
-            <input
-              type="text"
-              id="dropoffPlace"
-              name="dropoffPlace"
-              value={formData.dropoffPlace}
-              onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
-            />
-          </label>
-        </div>
+            <label htmlFor="pickupPlace">
+              <span className="text-sm">Pickup Place:</span>
+              <input
+                type="text"
+                id="pickupPlace"
+                name="pickupPlace"
+                value={formData.pickupPlace}
+                onChange={handleChange}
+                className="border border-gray-300 p-2 w-full"
+              />
+            </label>
 
-        {/* Buttons */}
-        <div className="flex  justify-center gap-2 ">
-          <button onClick={handleClose} className="w-full px-4 py-2 border border-gray-300 rounded-md">
-            Cancel
-          </button>
-          <button onClick={handleBook} className="w-full px-4 py-2 bg-black text-white rounded-md">
-            Book Now
-          </button>
-        </div>
+            <label htmlFor="dropoffPlace">
+              <span className="text-sm">Dropoff Place:</span>
+              <input
+                type="text"
+                id="dropoffPlace"
+                name="dropoffPlace"
+                value={formData.dropoffPlace}
+                onChange={handleChange}
+                className="border border-gray-300 p-2 w-full"
+              />
+            </label>
+
+            <div className="flex justify-end mt-4">
+              <button onClick={() => setStep(step - 1)} className="mr-2 px-4 py-2 border border-gray-300 rounded-md">
+                Back
+              </button>
+              <button onClick={handleBook} className="px-4 py-2 bg-black text-white rounded-md">
+                Book Now
+              </button>
+            </div>
+          </div>
+        )}
       </div>
-    </div >
+    </div>
   );
 };
 
